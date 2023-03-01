c05ca42b9819fbec550a267e3db012e534724b42
junaid033 <skjunaid0306@gmail.com>
2023-03-01 15:55:36 +0530

    update

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+AOS.init();
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  
+  
+  offset: 120,
+  delay: 0,
+  duration: 700, 
+  easing: 'ease', 
+  once: false, 
+  mirror: false, 
+  anchorPlacement: 'top-bottom', 
+
+});
